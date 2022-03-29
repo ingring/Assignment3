039b3055f260f03eea3837d6c765af158ea8c686
thomasks <thomasks@example.com>
2022-03-29 18:50:14 +0200

    added animated waterdroplet sidebar

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,44 @@
+// store background filenames in array
 const backgrounds = [];
 for (let i = 2; i <= 10; i++) {
 	backgrounds.push(`storyboard/img/background-img/scene${i}.JPEG`);
 }
-
 const cover_im = document.getElementById("cover");
+
+// add droplet sidebar droplets
+let sidebar = document.getElementById("drop_sidebar");
+for (let i = 0; i < 9; i++) {
+	sidebar.innerHTML += `<img src="storyboard/img/waterdrops/white.png" alt="" class="droplet">`;
+}
+
+function update_sidebar(active_droplet) {
+	let droplets = document.querySelectorAll("#drop_sidebar .droplet");
+	// iterate waterdroplets
+	for (let [i, droplet] of Object.entries(droplets)) {
+
+		if (i == active_droplet) {
+			// add ".active" to droplet
+			droplet.classList.add("active");
+		}
+		else {
+			droplet.classList.remove("active");
+		}
+
+		if (i < active_droplet) {
+			// add ".hidden" to droplet
+			droplet.classList.add("hidden")
+		}
+		else {
+			droplet.classList.remove("hidden")
+		}
+	}
+}
 
 const observer = new IntersectionObserver(
 	function (entries, observer) {
 		entries.forEach((entry) => {
 
 			if (entry.isIntersecting) {
-				// console.log(entry.target.innerHTML);
 				// observer.unobserve(entry.target);
 
 				// log what slide number this is
@@ -18,7 +46,10 @@ const observer = new IntersectionObserver(
 				console.log(slide_index);
 
 				// update background
-				cover_im.setAttribute("src", backgrounds[slide_index-1]);
+				cover_im.setAttribute("src", backgrounds[slide_index]);
+
+				// update sidebar droplets
+				update_sidebar(slide_index);
 
 			}
 
@@ -26,7 +57,7 @@ const observer = new IntersectionObserver(
 	}, 
 	{
 		treshold: 0,
-		rootMargin: "-100px"
+		rootMargin: "-250px"
 	}
 );
 
